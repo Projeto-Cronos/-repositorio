d11d9313aad71aee2d7f82ab79351fd42cc0192c
Vitor Brasil <vitorlbrasil@hotmail.com>
2022-09-01 23:40:31 -0300

    feat: modifying SideBar component and adding assets/icons folder

--- src/components/HeaderDashboard/SideBar/index.jsx
+++ src/components/HeaderDashboard/SideBar/index.jsx
@@ -1,13 +1,39 @@
 import { Link } from "react-router-dom";
 import { Container } from "./style";
+import calculadora from "../../../assets/icons/calculadora.svg";
+import projetos from "../../../assets/icons/projetos.svg";
+import rastreador from "../../../assets/icons/rastreador.svg";
+import temaEscuro from "../../../assets/icons/temaEscuro.svg";
+import configuracoes from "../../../assets/icons/configuracoes.svg";
+import sair from "../../../assets/icons/sair.svg";
 
 const SideBar = ({ isSideBarVisible }) => {
   return (
     <Container isSideBarVisible={isSideBarVisible}>
-      <Link to="/">Sobre</Link>
-      <Link to="/register">Cadastro</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/blog">Blog</Link>
+      <Link to="/">
+        <img src={rastreador} alt="" />
+        Rastreador de tempo
+      </Link>
+      <Link to="/register">
+        <img src={calculadora} alt="" />
+        Calculadora
+      </Link>
+      <Link to="/login">
+        <img src={projetos} alt="" />
+        Projetos
+      </Link>
+      <Link to="/blog">
+        <img src={configuracoes} alt="" />
+        Configurações
+      </Link>
+      <Link to="/blog">
+        <img src={temaEscuro} alt="" />
+        Tema escuro
+      </Link>
+      <Link to="/blog">
+        <img src={sair} alt="" />
+        Sair
+      </Link>
     </Container>
   );
 };
--- src/components/HeaderDashboard/SideBar/style.js
+++ src/components/HeaderDashboard/SideBar/style.js
@@ -9,31 +9,20 @@ export const Container = styled.div`
   position: fixed;
   height: 100%;
   top: 64px;
-  width: 160px;
+  width: 280px;
   left: ${(props) => (props.isSideBarVisible ? "0" : "-100%")};
   background-color: var(--white);
   box-shadow: 0px 20px 32px rgba(0, 0, 0, 0.25);
   animation: showSidebar 0.4s;
 
-  @media screen and (min-width: 950px) {
-    position: initial;
-    width: 100%;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    box-shadow: initial;
-    background-color: transparent;
-  }
-
   img {
-    height: 20px;
-
-    @media screen and (min-width: 950px) {
-      display: none;
-    }
+    width: 20px;
   }
 
   a {
+    display: flex;
+    align-items: center;
+    gap: 16px;
     font-family: "Inter", sans-serif;
     font-size: 20px;
     font-weight: 500;
