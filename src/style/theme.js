c8879d840dfd30b5d04441aff7df7abff165ef32
Vagner <vagmengali2015@gmail.com>
2022-09-07 13:43:24 -0300

    feat: adding full dark theme

--- src/style/theme.js
+++ src/style/theme.js
@@ -21,23 +21,23 @@ const lightTheme = {
   
   // Objeto com a definição do estilo para o tema darkTheme
   const darkTheme = {
-    white:"#181a1b",
-    black: "#e8e6e3",
-    grey1: "#383838",
-    grey2: "#9e9689",
-    grey3: "#FAFAFA",
-    grey4: "#180a1b",
-    grey5: "#D9D9D9",
-    orange1: "#bf4c07",
-    orange2: "#b22f14",
+    white:"#0E110F",
+    black: "#FFFFFF",
+    grey1: "#FFFFFF",
+    grey2: "#454442",
+    grey3: "#272727",
+    grey4: "#131313",
+    grey5: "#747474",
+    orange1: "#F66813",
+    orange2: "#DF3B19",
     blue1: "#2927A6",
     blue2: "#272586",
     blue3: "#155BCB",
-    red1: "#ff2b2b",
-    red2: "#ff2121",
+    red1: "#E60000",
+    red2: "#F40000",
     green1: "#3FE864",
     yellow1: "#FFCD07",
-    shadow: "rgba(0, 0, 0, 0.6)",
+    shadow: "rgba(255, 255, 255, 0.6)",
   };
   
   export const themes = {
